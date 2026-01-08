6d3d1316e7619f19be3e4f2bbef7bb15105407d2
Mirza Anjuman <anjuman@gmail.com>
2026-01-09 00:17:30 +0600

    update server to use PORT from environment variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
-
-const PORT = 2000;
+require("dotenv").config();
 
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`http://localhost:${process.env.PORT}`);
 });
