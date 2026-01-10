e7ef6f1279000eb320a80f99231ec3c212933de5
Mirza Anjuman <anjuman@gmail.com>
2026-01-11 00:40:07 +0600

    feat: create Home component to structure the main page layout

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import SearchBar from '../components/SearchBar';
+import Categories from '../components/Categories';
+import FeaturedRestaurants from '../components/FeaturedRestaurants';
+import HowItWorks from '../components/HowItWorks';
+import Features from '../components/Features';
+import CTA from '../components/CTA';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar />
+      <HeroSection />
+      <SearchBar />
+      <Categories />
+      <FeaturedRestaurants />
+      <HowItWorks />
+      <Features />
+      <CTA />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
