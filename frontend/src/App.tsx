47c25cb8ed9d12d10b2635f9cdd69e77d0f047fd
Mirza Anjuman <anjuman@gmail.com>
2026-01-30 19:55:15 +0600

    feat: restructure routing in App component for improved layout organization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import ForgotPassword from './pages/ForgotPassword';
-import NewHomePage from './pages/NewHomePage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import VerifyEmail from './pages/VerifyEmail';
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { RootLayout, MainLayout, AuthLayout } from "./layouts";
+
+// Pages
+import NewHomePage from "./pages/NewHomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import VerifyEmail from "./pages/VerifyEmail";
 
 function App(): React.ReactElement {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<NewHomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
+        {/* Root Layout wraps everything */}
+        <Route element={<RootLayout />}>
+          {/* Public pages with Navbar/Footer */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<NewHomePage />} />
+          </Route>
+
+          {/* Auth pages with split-screen layout */}
+          <Route element={<AuthLayout />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/verify-email" element={<VerifyEmail />} />
+          </Route>
+        </Route>
       </Routes>
     </Router>
   );
